d1c626daa5741df4cb466eb3ffb4d55ff6996667
lilirdz <lilimrod@gmail.com>
2020-06-17 15:36:23 -0500

    now able to create a new user

--- job-finder-frontend/src/user.js
+++ job-finder-frontend/src/user.js
@@ -1,24 +1,26 @@
 const userUrl = "http://localhost:3000/users";
 logInUser();
+createUser();
+
 function myFetch(url, options = {}) {
   return fetch(url, options).then((res) => res.json());
 }
 
 //from Raul
-fetch("http://localhost:3000/users_signin", {
-  method: "POST",
-  headers: { "Content-Type": "application/json", Accept: "application/json" },
-  body: JSON.stringify({ name: "Bob" }),
-})
-  .then((res) => res.json())
-  .then(console.log);
+// fetch("http://localhost:3000/users_signin", {
+//   method: "POST",
+//   headers: { "Content-Type": "application/json", Accept: "application/json" },
+//   body: JSON.stringify({ name: "Bob" }),
+// })
+//   .then((res) => res.json())
+//   .then(console.log);
 
 function logInUser() {
   const logInForm = document.createElement("form");
   logInForm.classList.add("initial-log-in-form");
   const welcomeP = document.createElement("p");
   const enterNameLabel = document.createElement("label");
-  enterNameLabel.innerHTML =
+  welcomeP.innerHTML =
     "Enter your name and we'll pull up your job search tracker. If you're new, we'll get you started.";
   enterNameLabel.setAttribute("for", "username");
   const typeInName = document.createElement("input");
@@ -56,8 +58,9 @@ function logInUser() {
   });
 
   logInForm.append(welcomeP, enterNameLabel, typeInName, submitName);
-  document.querySelector("h2").append(logInForm);
+  document.querySelector(".page-banner").append(logInForm);
 }
+
 function showUser(user) {
   const username = document.querySelector("#username");
   username.textContent = `Hello ${user.name}!`;
@@ -90,7 +93,7 @@ function showUser(user) {
   });
 }
 
-// Get the modal
+// Modal for a user to Edit their info
 const userModal = document.getElementById("userModal");
 // Get the button that opens the modal
 const updateBtn = document.getElementById("edit-btn");
@@ -115,3 +118,51 @@ window.onclick = function (event) {
     userModal.style.display = "none";
   }
 };
+
+function createUser() {
+  const welcome = document.createElement("p");
+  welcome.textContent = "Create account here";
+
+  const form = document.createElement("form");
+  form.classList.add("new-user-form");
+
+  const label = document.createElement("label");
+  label.setAttribute("for", "name");
+
+  const inputName = document.createElement("input");
+  inputName.setAttribute("type", "text");
+  inputName.setAttribute("id", "new-name");
+
+  const submitBtn = document.createElement("input");
+  submitBtn.setAttribute("type", "submit");
+  submitBtn.setAttribute("value", "Submit");
+
+  const spacing = document.createElement("br");
+
+  form.append(label, inputName, spacing, submitBtn);
+
+  const pageBanner = document.querySelector(".page-banner");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newUserName = document.querySelector("#new-name").value;
+    const postOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: newUserName,
+      }),
+    };
+
+    fetch(userUrl, postOptions)
+      .then((res) => res.json())
+      .then((user) => {
+        showUser(user);
+      });
+  });
+
+  pageBanner.append(welcome, form);
+}
